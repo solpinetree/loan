bf0192c2d64853b1b6061560187c308b67de1eb2
sol <sadlthf@icloud.com>
2023-04-27 14:42:58 +0900

    #1 - ResponseDTO
    최종으로 ResultObject(ResultType), api return data 를 묶어서 return 해주는 responseDTO

---
+++ src/main/java/com/sol/loan/dto/ResponseDTO.java
@@ -0,0 +1,43 @@
+package com.sol.loan.dto;
+
+import com.sol.loan.exception.BaseException;
+import lombok.*;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDTO<T> implements Serializable {
+
+    private ResultObject result;
+
+    private T data;
+
+    public ResponseDTO(ResultObject result) {
+        this.result = result;
+    }
+
+    public ResponseDTO(T data) {
+        this.data = data;
+    }
+
+    public static <T> ResponseDTO<T> ok() {
+        return new ResponseDTO<>(ResultObject.getSuccess());
+    }
+
+    public static <T> ResponseDTO<T> ok(T data) {
+        return new ResponseDTO<>(ResultObject.getSuccess(), data);
+    }
+
+    public static <T> ResponseDTO<T> response(T data) {
+        return new ResponseDTO<>(ResultObject.getSuccess(), data);
+    }
+
+    public ResponseDTO(BaseException ex) {
+        this.result = new ResultObject(ex);
+    }
+}
