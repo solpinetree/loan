43a003e6895465693d753282f67a6f211b5df3ec
sol <sadlthf@icloud.com>
2023-05-04 14:09:47 +0900

    #5 - controller 구현

--- src/main/java/com/sol/loan/controller/CounselController.java
+++ src/main/java/com/sol/loan/controller/CounselController.java
@@ -5,10 +5,7 @@ import com.sol.loan.dto.CounselDTO.Response;
 import com.sol.loan.dto.ResponseDTO;
 import com.sol.loan.service.CounselService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,5 +17,10 @@ public class CounselController extends AbstractController {
     @PostMapping
     public ResponseDTO<Response> create(@RequestBody Request request) {
         return ok(counselService.create(request));
+    }
+
+    @GetMapping("/{counselId}")
+    public ResponseDTO<Response> get(@PathVariable Long counselId) {
+        return ok(counselService.get(counselId));
     }
 }
