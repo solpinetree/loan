0d1bf7baf79b5e85954abe6dfda8041ccd8cc2a6
sol <sadlthf@icloud.com>
2023-05-04 14:07:54 +0900

    #5 - 조회 비즈니스 로직 구현
    일단 에러는 다 `SYSTEM_ERROR` 로 반환하고
    후에 고도화할 예정
    
    CounselService 외에 있는 로직은 제외 시키기 위해 mockup(when, thenReturn)를 한다.

--- src/main/java/com/sol/loan/service/CounselService.java
+++ src/main/java/com/sol/loan/service/CounselService.java
@@ -6,4 +6,6 @@ import com.sol.loan.dto.CounselDTO.Response;
 public interface CounselService {
 
     Response create(Request request);
+
+    Response get(Long counselId);
 }
--- src/test/java/com/sol/loan/service/CounselServiceTest.java
+++ src/test/java/com/sol/loan/service/CounselServiceTest.java
@@ -3,7 +3,10 @@ package com.sol.loan.service;
 import com.sol.loan.domain.Counsel;
 import com.sol.loan.dto.CounselDTO.Request;
 import com.sol.loan.dto.CounselDTO.Response;
+import com.sol.loan.exception.BaseException;
+import com.sol.loan.exception.ResultType;
 import com.sol.loan.repository.CounselRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,9 +16,11 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("비즈니스 로직 테스트 - 대출 상담 기능")
 @ExtendWith(MockitoExtension.class)
@@ -34,28 +39,54 @@ class CounselServiceTest {
     @Test
     void Should_ReturnResponseOfNewCounselEntity_When_RequestCounsel() {
         Counsel entity = Counsel.builder()
-            .name("Member 1")
-            .cellPhone("010-1111-1111")
-            .email("abx@def.h")
-            .memo("대출을 희망합니다.")
-            .zipCode("12345")
-            .address("경기도 수원시 떙떙동")
-            .addressDetail("111동 111호")
-            .build();
+                .name("Member 1")
+                .cellPhone("010-1111-1111")
+                .email("abx@def.h")
+                .memo("대출을 희망합니다.")
+                .zipCode("12345")
+                .address("경기도 수원시 떙떙동")
+                .addressDetail("111동 111호")
+                .build();
 
         Request request = Request.builder()
-            .name("Member 1")
-            .cellPhone("010-1111-1111")
-            .email("abx@def.h")
-            .memo("대출을 희망합니다.")
-            .zipCode("12345")
-            .address("경기도 수원시 떙떙동")
-            .addressDetail("111동 111호")
-            .build();
+                .name("Member 1")
+                .cellPhone("010-1111-1111")
+                .email("abx@def.h")
+                .memo("대출을 희망합니다.")
+                .zipCode("12345")
+                .address("경기도 수원시 떙떙동")
+                .addressDetail("111동 111호")
+                .build();
         when(counselRepository.save(any(Counsel.class))).thenReturn(entity);
 
         Response actual = counselService.create(request);
 
         assertThat(actual.getName()).isSameAs(entity.getName());
+    }
+
+    @DisplayName("대출 상담 조회 기능 테스트 - 레코드 O")
+    @Test
+    void Should_ReturnResponseOfExistCounselEntity_When_RequestExistCounselId() {
+        Long findId = 1L;
+
+        Counsel entity = Counsel.builder()
+                .counselId(1L)
+                .build();
+
+        when(counselRepository.findById(findId)).thenReturn(Optional.ofNullable(entity));
+
+        Response actual = counselService.get(findId);
+
+        assertThat(actual.getCounselId()).isSameAs(findId);
+    }
+
+    @DisplayName("대출 상담 조회 기능 테스트 - 레코드 x")
+    @Test
+    void Should_ThrowException_When_RequestNotExistCounselId() {
+        Long findId = 2L;
+
+        when(counselRepository.findById(findId)).thenThrow(new BaseException(ResultType.SYSTEM_ERROR));
+
+        Assertions.assertThrows(BaseException.class, () -> counselService.get(findId));
     }
 }
