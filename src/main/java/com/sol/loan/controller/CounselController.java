03f197d848a5ebb5f7f40edff71d6fd9769460d6
sol <sadlthf@icloud.com>
2023-05-06 01:17:43 +0900

    #6 - 수정 controller method 추가 및 구현

--- src/main/java/com/sol/loan/controller/CounselController.java
+++ src/main/java/com/sol/loan/controller/CounselController.java
@@ -23,4 +23,9 @@ public class CounselController extends AbstractController {
     public ResponseDTO<Response> get(@PathVariable Long counselId) {
         return ok(counselService.get(counselId));
     }
+
+    @PutMapping("/{counselId}")
+    public ResponseDTO<Response> update(@PathVariable Long counselId, @RequestBody Request request) {
+        return ok(counselService.update(counselId, request));
+    }
 }
